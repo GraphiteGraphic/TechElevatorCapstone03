363aca43f9f507ccc94da4b5f781a984eceda510
Ahmad Doleh <ahmad@doleh.com>
2021-04-13 14:12:21 -0400

    recipe list now displays cards with drop shadows. Waiting on image implementation

--- vue/src/components/recipeList.vue
+++ vue/src/components/recipeList.vue
@@ -1,27 +1,46 @@
 <template>
   <div>
-    <router-link
-      :to="{ name: 'recipe', params: { id: recipe.recipeId } }"
-      v-for="recipe in recipeList"
-      :key="recipe.recipeId"
-      :name="recipe.recipeName"
-      :instructions="recipe.instructions"
-      >{{ recipe.recipeName }}</router-link
-    >
+    <section v-for="recipe in recipeList" :key="recipe.recipeId">
+      <router-link
+        :to="{ name: 'recipe', params: { id: recipe.recipeId } }"
+        :class="recipe.recipeName"
+        >{{ recipe.recipeName }}</router-link
+      >
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'recipe-list',
-    props: {
-        recipeList: Array
-    }
+  name: "recipe-list",
+  props: {
+    recipeList: Array,
+  },
 };
 </script>
 
 <style scoped>
-a{
-    display: block;
+div {
+  display: flex;
+  flex-wrap: wrap;
+}
+section{
+  width: 300px;
+  height: 400px;
+  background-color: blue;
+  margin: 2px;
+}
+a {
+  display: inline-block;
+  width: 300px;
+  height: 400px;
+  background-color: rgb(125, 125, 255);
+  position: relative;
+  left: -1px;
+  top: -1px;
+}
+a:hover {
+  left: -3px;
+  top: -3px;
 }
 </style>
