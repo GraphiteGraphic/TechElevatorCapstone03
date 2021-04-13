c3d1932e5fba127decce471186221761089ca6cd
tibecker <timothy.j.becker@outlook.com>
2021-04-13 10:15:32 -0400

    Implemented component to handle uploading photos

--- vue/src/views/AddRecipe.vue
+++ vue/src/views/AddRecipe.vue
@@ -2,16 +2,19 @@
   <div>
     <new-recipe v-if="!recipeId"/>
     <edit-recipe v-if="recipeId"/>
+    <photos />
   </div>
 </template>
 
 <script>
 import EditRecipe from '../components/EditRecipe.vue'
 import newRecipe from "../components/NewRecipe.vue"
+import Photos from '../components/Photos.vue';
 export default {
   components: {
     newRecipe,
     EditRecipe,
+    Photos,
   },
   computed: {
     recipeId(){
